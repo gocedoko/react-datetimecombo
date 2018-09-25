0960a2dc319eb5a2e3c7ead479654f135decf3a3
Goce Dokoski <dokoski.g@quipugmbh.com>
2018-09-25 12:16:41 +0200

    add controlled and uncontrolled component in example

--- example/test.jsx
+++ example/test.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import DateTimeCombo from '../DateTimeCombo.jsx'
+import moment from 'moment'
 
 import '../react-datetimecombo.css'
 
-ReactDOM.render(<DateTimeCombo />, 
+class DateTimeComboTest extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {time: moment()}
+	}
+
+	render(){
+		console.log("Datetime in state: " + this.state.time.format("DD.MM.YYYY"))
+
+		return <span>
+			<small>Controlled</small>
+			<DateTimeCombo 
+				value={this.state.time}
+				onChange={v => this.setState({time: v})}
+			/>
+
+			<br/>
+
+			<small>Uncontrolled</small>
+			<DateTimeCombo />
+		</span>
+		
+		
+	}
+
+}
+
+ReactDOM.render(<DateTimeComboTest 
+
+/>, 
 	document.getElementById('datetimecombo')
 );
